0fdfdb61f2a46544b5d28a5fd5557b5cef77600f
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-24 01:48:51 -0300

    Create ex02.js

---
+++ aula-02/ex02.js
@@ -0,0 +1,14 @@
+/*let idade =input("Qual a sua idade ?");
+if (idade>=18){
+    console.log("maior de idade !")
+    }
+    else{
+        console.log("menor de idade !")
+    }*/
+
+    let count = 6;
+    while (count < 5){
+        console.log(`usuario ${count}`);
+        count = count + 1
+
+    };
