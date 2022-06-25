acd8c4c5ad538a443e8f71c58762c44892f7fcd6
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-25 00:46:36 -0300

    ex05

---
+++ aula-02/aula-04/js/main.js
@@ -0,0 +1,29 @@
+function soma(n1,n2){
+    return n1+n2
+}
+
+function validaIdade(idade){
+    let validar
+    if (idade >= 18){
+        validar =true
+        alert(`Com a idade de ${idade} anos a pessoa é MAIOR`)}
+               
+        
+    else{
+        validar = false
+        alert(`Com a idade de ${idade} anos a pessoa é MENOR` )}
+    
+    return validar }
+
+
+let idade = prompt("Qual sua idade ? :")
+
+    validaIdade(idade)
+    
+        
+
+/*function setReplace(frase, nome, novo_nome){
+return frase.replace(nome, novo_nome)
+}
+alert(soma(5,10))
+alert(setReplace("vai japao", "japao", "Brasil"))*/
