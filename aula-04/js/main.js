0226c7f06e25892fa62e06749424d28347c64c65
Tony Gama <97718437+TonyRio@users.noreply.github.com>
2022-06-24 22:02:40 -0300

    Update main.js

--- aula-04/js/main.js
+++ aula-04/js/main.js
@@ -5,15 +5,23 @@ function soma(n1,n2){
 function validaIdade(idade){
     let validar
     if (idade >= 18){
-        validar =true}
+        validar =true
+        alert(`Com a idade de ${idade} anos a pessoa é MAIOR`)}
+               
+        
     else{
         validar = false
-    }
-    return validar
-}
+        alert(`Com a idade de ${idade} anos a pessoa é MENOR` )}
+    
+    return validar }
+
 
 let idade = prompt("Qual sua idade ? :")
-alert(validaIdade(idade))
+
+    validaIdade(idade)
+    
+        
+
 /*function setReplace(frase, nome, novo_nome){
 return frase.replace(nome, novo_nome)
 }
